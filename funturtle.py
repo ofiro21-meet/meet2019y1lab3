6f33b22c40ee114fec199319de55fefc13d9fa5e
ofiro21-meet <ofiro21@meet.mit.edu>
2019-07-24 07:29:27 -0400

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,18 @@
+import turtle
+turtle.shape('turtle')
+finn=turtle.clone()
+finn.shape('square')
+finn.penup
+finn.goto(-100,-100)
+finn.goto(-100,100)
+finn.goto(100,100)
+finn2=turtle.clone()
+finn2.panup
+finn2.goto(00)
+finn2.shape('arrow')
+finn2.pendown
+finn2.goto(100,100)
+finn2.shape('tringle')
+finn2.goto(200,0)
+finn2.shape('circle')
+finn2.goto(0,0)
